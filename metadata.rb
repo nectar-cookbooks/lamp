5084cb0f8697bb68921acd2902f7947455da421c
Stephen Crawley <s.crawley@uq.edu.au>
2014-07-16 09:50:29 +1000

    First checking ...

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name "setup"
+maintainer "Stephen Crawley"
+maintainer_email "s.crawley at uq dot edu dot au"
+license "BSD"
+description "Generic LAMP - linux / Apache / Perl / PHP / MySQL"
+version "0.0.1"
+
+depends "apache2"
+depends "php"
+depends "mysql"
+
