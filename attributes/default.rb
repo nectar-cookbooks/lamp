8e516b193a806d137c3f8d424a278ab78178cde5
Stephen Crawley <s.crawley@uq.edu.au>
2014-07-23 15:04:39 +1000

    More work on sample template

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,9 +10,16 @@ node.default['lamp']['pmwiki']['pub_dir_url'] = nil
 node.default['lamp']['pmwiki']['admin_password'] = nil
 node.default['lamp']['pmwiki']['upload_password'] = nil
 node.default['lamp']['pmwiki']['enable_upload'] = false
+node.default['lamp']['pmwiki']['upload_perm_add'] = 0
 node.default['lamp']['pmwiki']['enable_diag'] = false
 node.default['lamp']['pmwiki']['enable_ims_caching'] = false
+node.default['lamp']['pmwiki']['enable_creole'] = false
+node.default['lamp']['pmwiki']['enable_wikiwords'] = false
+node.default['lamp']['pmwiki']['space_wikiwords'] = false
 node.default['lamp']['pmwiki']['tz'] = nil
 node.default['lamp']['pmwiki']['blocklist'] = nil # e.g. 1 or 10 -
                                                   # see PmWiki.Blocklist
+node.default['lamp']['pmwiki']['ws_pre'] = 0
+node.default['lamp']['pmwiki']['diff_keep_days'] = nil
+node.default['lamp']['pmwiki']['enable_page_list_protect'] = true
 node.default['lamp']['pmwiki']['skin'] = nil
