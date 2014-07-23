1311ef8a4b2bb87a1cb86b7f36ab255a45b97023
Stephen Crawley <s.crawley@uq.edu.au>
2014-07-23 10:26:46 +1000

    Make the logo URL optional

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,7 @@ node.default['lamp']['pmwiki']['version'] = 'pmwiki-latest' # any version sans t
 node.default['lamp']['pmwiki']['action'] = 'install' # or 'upgrade'
 
 node.default['lamp']['pmwiki']['wiki_title'] = 'Default Wiki Title'
-node.default['lamp']['pmwiki']['page_logo_url'] = 
-    'https://upload.wikimedia.org/wikipedia/commons/a/af/Question_mark.png'
+node.default['lamp']['pmwiki']['page_logo_url'] = nil
 node.default['lamp']['pmwiki']['script_url'] = nil 
 node.default['lamp']['pmwiki']['pub_dir_url'] = nil
 node.default['lamp']['pmwiki']['admin_password'] = nil
